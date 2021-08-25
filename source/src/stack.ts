f3dd5c6106bf52bc3b60a7c9e47a120d2bcac4e8
George Gu <minggu@amazon.com>
2021-08-25 16:17:54 +0800

    update the lambda

--- source/src/stack.ts
+++ source/src/stack.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
-import {Construct, Stack, StackProps,  CfnParameter, CfnParameterProps } from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 import * as lambda from '@aws-cdk/aws-lambda';
+import { Construct, Stack, StackProps, CfnMapping, CfnParameter, CfnParameterProps, Aws } from '@aws-cdk/core';
 
 export class SolutionStack extends Stack {
   private _paramGroup: { [grpname: string]: CfnParameter[]} = {}
@@ -30,95 +31,35 @@ export class SolutionStack extends Stack {
   }
 }
 
-// class MyImage implements ec2.IMachineImage {
-//   private mapping: { [k1: string]: { [k2: string]: any } } = {};
-//   constructor(readonly amiMap: { [region: string]: string }) {
-//     for (const [region, ami] of Object.entries(amiMap)) {
-//       this.mapping[region] = { ami };
-//     }
-//   }
-//   public getImage(parent: Construct): ec2.MachineImageConfig {
-//     const amiMap = new CfnMapping(parent, 'AmiMap', { mapping: this.mapping });
-//     return {
-//       imageId: amiMap.findInMap(Aws.REGION, 'ami'),
-//       userData: ec2.UserData.forLinux(),
-//       osType: ec2.OperatingSystemType.LINUX,
-//     };
-//   }
-// }
-
 export class IntelligentCaptchaStack extends SolutionStack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    // const vpc= new ec2.Vpc(this, 'VPC');
-    //
-    // new ec2.Instance(this, 'Instance', {
-    //   vpc,
-    //   instanceType: new ec2.InstanceType('t2.micro'),
-    //   machineImage: new MyImage({
-    //     'cn-north-1': 'ami-cn-north-1',
-    //     'cn-northwest-1': 'ami-cn-northwest-1',
-    //   }),
-    // });
     this.setDescription("(SO####) - Intelligent Captcha stack.");
 
-    const layer = new lambda.LayerVersion(this, 'MyLayer', {
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/'), {
-        bundling: {
-          image: lambda.Runtime.NODEJS_14_X.bundlingImage,
-          command: [
-            'bash', '-xc', [
-              'export npm_config_update_notifier=false',
-              'export npm_config_cache=$(mktemp -d)', // https://github.com/aws/aws-cdk/issues/8707#issuecomment-757435414
-              'cd $(mktemp -d)',
-              'cp -v /asset-input/package*.json .',
-              'npm i --only=prod',
-              'mkdir -p /asset-output/nodejs/',
-              'cp -au node_modules /asset-output/nodejs/',
-            ].join('&&'),
-          ],
-        },
-      }),
-      compatibleRuntimes: [lambda.Runtime.NODEJS_14_X],
-      description: 'A layer to test the L2 construct',
+    const api = new apigateway.RestApi(this, 'api', {
+      description: 'example api gateway',
+      deployOptions: {
+        stageName: 'dev',
+      },
     });
 
-    new lambda.Function(this, 'CaptchaHandler', {
+    new cdk.CfnOutput(this, 'apiUrl', {value: api.url});
+
+    // define get captcha function
+    const getCaptchaLambda = new lambda.Function(this, 'get-captcha-lambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda.d/captchaGenerator')),
       handler: 'getCaptcha',
-      layers: [layer],
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../lambda.d/captchaGenerator')),
     });
 
+    // add a /captcha resource
+    const todos = api.root.addResource('captcha');
 
-    //Real things adding here
-
-    // const captchaFnRole = new Role(this, 'TokenFuncRole', {
-    //   assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-    //   managedPolicies: [
-    //     ManagedPolicy.fromAwsManagedPolicyName(
-    //       'service-role/AWSLambdaBasicExecutionRole',
-    //     ),
-    //   ],
-    // });
-    // // Create a lambda that returns captcha results
-    // const captchaGenerator = new NodejsFunction(, 'captchaGenerator', {
-    //   entry: path.join(__dirname, '../lambda.d/captchaGenerator/captcha.ts'),
-    //   handler: 'getCaptcha',
-    //   timeout: Duration.seconds(30),
-    //   memorySize: 512,
-    //   role: captchaFnRole,
-    //   runtime: Runtime.NODEJS_14_X,
-    //   tracing: Tracing.ACTIVE,
-    // });
-    //
-    // const captchaFnIntegration = new LambdaProxyIntegration({
-    //   handler: captchaGenerator,
-    //   payloadFormatVersion: PayloadFormatVersion.VERSION_2_0,
-    // });
-
-
-
+    // integrate GET /captcha with getCaptchaLambda
+    todos.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getCaptchaLambda, {proxy: true}),
+    );
   }
 }

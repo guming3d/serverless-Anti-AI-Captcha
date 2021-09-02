bccd9de42186fbe30e30004acc628f287ef4c1e4
George Gu <minggu@amazon.com>
2021-09-02 10:27:06 +0800

    adding ECS job for generating captchas

--- source/src/captcha-generator.ts
+++ source/src/captcha-generator.ts
@@ -1,7 +1,14 @@
-import { Construct,  NestedStack, NestedStackProps } from '@aws-cdk/core';
+import {Construct, IgnoreMode, NestedStack, NestedStackProps} from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
+import * as events from "@aws-cdk/aws-events";
+import * as iam from "@aws-cdk/aws-iam";
+import { DockerImageAsset } from '@aws-cdk/aws-ecr-assets';
+import {ContainerImage} from "@aws-cdk/aws-ecs";
+import * as path from "path";
+import * as logs from "@aws-cdk/aws-logs";
+
 
 export class CaptchaGeneratorStack extends NestedStack {
 
@@ -16,25 +23,58 @@ export class CaptchaGeneratorStack extends NestedStack {
     });
 
     //TODO:need to implement
+    const taskDefinitionDailyGenerateCaptcha= new ecs.FargateTaskDefinition(this, "DailyGenerateCaptchaFargateTask", {
+      memoryLimitMiB: 2048,
+      cpu: 512
+    })
+
+
+    const dailyCaptchaGeneratorImage = new DockerImageAsset(this, 'BulkLoadGraphDataImage', {
+      directory: path.join(__dirname, '../'),
+      file: 'container.d/load-captcha-data/Dockerfile',
+      exclude: [
+        'container.d/(!load-captcha-data)',
+        'lambda.d/**',
+        'lib/**',
+        'scripts/**',
+      ],
+      ignoreMode: IgnoreMode.GLOB,
+    });
+
+
+    taskDefinitionDailyGenerateCaptcha.addContainer("DailyCaptchaGeneratorContainer", {
+      image: ContainerImage.fromDockerImageAsset(dailyCaptchaGeneratorImage),
+      cpu: 512,
+      memoryLimitMiB: 2048,
+      memoryReservationMiB: 2048,
+      logging: ecs.LogDriver.awsLogs({
+        streamPrefix: "ecs",
+        logGroup: new logs.LogGroup(this, "DailyGenerateCaptchaLogGroup", {
+          logGroupName: "/ecs/DailyGenerateCaptchaLogGroup/ContainerLogs",
+          retention: logs.RetentionDays.ONE_WEEK
+        })
+      }),
+      //entryPoint: ["python3", "src/live_conversion.py"],
+    })
+
+    const permissions = new iam.PolicyStatement({
+      actions: [
+        "s3:*",
+        "cloudwatch:*",
+        "dynamodb:*"
+      ],
+      resources: ["*"]
+    })
+
+    taskDefinitionDailyGenerateCaptcha.addToTaskRolePolicy(permissions);
     new ecs_patterns.ScheduledFargateTask(this, "CaptchaGeneratorTask",{
       cluster: cluster,
       desiredTaskCount: 1,
       enabled: true,
-      ruleName: "",
-      schedule: undefined,
-      securityGroups: [],
-      subnetSelection: undefined,
-      vpc: vpc
+      schedule: events.Schedule.expression("cron(0/15 * * * ? *)"),
+      vpc: vpc,
+      scheduledFargateTaskDefinitionOptions: {taskDefinition: taskDefinitionDailyGenerateCaptcha}
+    });
 
-    });
-    // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-      cluster: cluster, // Required
-      cpu: 512, // Default is 256
-      desiredCount: 6, // Default is 1
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-      memoryLimitMiB: 2048, // Default is 512
-      publicLoadBalancer: true // Default is false
-    });
   }
 }

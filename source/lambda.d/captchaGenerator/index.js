19d45884af11f86b17f67e5593b00a12405a4709
George Gu <minggu@amazon.com>
2021-09-15 16:53:00 +0800

    update lambda fallback

--- source/lambda.d/captchaGenerator/index.js
+++ source/lambda.d/captchaGenerator/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-exports.handler =  function(event, context, callback) {
+exports.handler = function (event, context, callback) {
 
   // Load the AWS SDK for Node.js
   let AWS = require('aws-sdk');
@@ -12,17 +12,23 @@ exports.handler =  function(event, context, callback) {
   let ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
   //get current date with yyyymmdd format
-  function dateFormat (date, fstr, utc) {
+  function dateFormat(date, fstr, utc) {
     utc = utc ? 'getUTC' : 'get';
-    return fstr.replace (/%[Ymd]/g, function (m) {
+    return fstr.replace(/%[Ymd]/g, function (m) {
       switch (m) {
-        case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
-        case '%m': m = 1 + date[utc + 'Month'] (); break;
-        case '%d': m = date[utc + 'Date'] (); break;
-        default: return m.slice (1); // unknown code, remove %
+        case '%Y':
+          return date[utc + 'FullYear'](); // no leading zeros required
+        case '%m':
+          m = 1 + date[utc + 'Month']();
+          break;
+        case '%d':
+          m = date[utc + 'Date']();
+          break;
+        default:
+          return m.slice(1); // unknown code, remove %
       }
       // add leading zero if required
-      return ('0' + m).slice (-2);
+      return ('0' + m).slice(-2);
     });
   }
 
@@ -31,14 +37,10 @@ exports.handler =  function(event, context, callback) {
     return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
   }
 
-  //TODO: replace tomorrowDate to current date in real production
-  let tomorrowDate = new Date();
-  tomorrowDate.setDate(tomorrowDate.getDate() + 1);
-
-  const convertedDate = convertTZ(tomorrowDate,"Asia/Shanghai")
-
-  let currentDate = dateFormat(convertedDate, "%Y%m%d", false);
-  console.debug("current date is " + currentDate);
+  let curDate = new Date();
+  let convertedDate = convertTZ(curDate, "Asia/Shanghai")
+  let currentDateStr = dateFormat(convertedDate, "%Y%m%d", false);
+  console.debug("current date is " + currentDateStr);
 
   //get a random index from 1 to MAXINDEX
   const MAXINDEX = process.env.MAX_DAILY_INDEX;
@@ -46,52 +48,64 @@ exports.handler =  function(event, context, callback) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
-  console.debug(
-    random(0, MAXINDEX-1)
-  )
-
   let ddbtablename = process.env.DDB_TABLE_NAME
   let params = {
     TableName: ddbtablename,
     Key: {
-      'captcha_date': {S: currentDate},
-      'captcha_index': {N: random(0, MAXINDEX-1).toString() }
+      'captcha_date': {S: currentDateStr},
+      'captcha_index': {N: random(0, MAXINDEX - 1).toString()}
     }
   };
 
-  ddb.getItem(params, function(err, data) {
+  ddb.getItem(params, function (err, data) {
     if (err) {
       console.error("Failed to get DDB record with error:", err);
-      //will try to get captcha from previous date
-      let prevDate = convertedDate.setDate(convertedDate.getDate() - 1);
-      let prevDateStr = dateFormat(prevDate, "%Y%m%d", false);
-      console.debug("prev date is " + prevDateStr);
-      params.Key.captcha_date = prevDateStr;
-      ddb.getItem(params, function(err, data) {
-        if (err) {
-          console.error("Failed to get DDB record with error:", err);
-          callback(Error(err));
-        } else {
-          console.debug("captchaUrl is ", data.Item.captchaUrl.S);
-          console.debug("result is ", data.Item.result.S);
-          callback(null,
-            {
-              statusCode: 200,
-              headers: { 'Content-Type': 'text/plain' },
-              body: JSON.stringify(data.Item)
-            });
-        }
-      });
-      // callback(Error(err));
+      callback(Error(err));
     } else {
-      console.debug("captchaUrl is ", data.Item.captchaUrl.S);
-      console.debug("result is ", data.Item.result.S);
-      callback(null,
-        {
-          statusCode: 200,
-          headers: { 'Content-Type': 'text/plain' },
-          body: JSON.stringify(data.Item)
+      if (data.Item == null) {
+        console.debug("failed to get the record fron dynamod db will try to get the record from previous day");
+
+        let previousDate = curDate;
+        previousDate.setDate(previousDate.getDate() - 1);
+        convertedDate = convertTZ(previousDate, "Asia/Shanghai");
+        let previousDateStr = dateFormat(convertedDate, "%Y%m%d", false);
+        console.debug("fallback date is ", previousDateStr);
+
+        params = {
+          TableName: ddbtablename,
+          Key: {
+            'captcha_date': {S: previousDateStr},
+            'captcha_index': {N: random(0, MAXINDEX - 1).toString()}
+          }
+        };
+        ddb.getItem(params, function (err, data) {
+          if (err) {
+            console.error("Failed to get DDB record with error:", err);
+            callback(Error(err));
+          } else {
+            if (data.Item == null) {
+              callback(Error("failed to get the record from dynamodb with date:"+previousDateStr));
+            } else {
+              console.debug("result is ", JSON.stringify(data));
+              callback(null,
+                {
+                  statusCode: 200,
+                  headers: {'Content-Type': 'text/plain'},
+                  body: JSON.stringify(data.Item)
+                });
+            }
+          }
         });
+
+      } else {
+        console.debug("result is ", JSON.stringify(data));
+        callback(null,
+          {
+            statusCode: 200,
+            headers: {'Content-Type': 'text/plain'},
+            body: JSON.stringify(data.Item)
+          });
+      }
     }
   });
 };
